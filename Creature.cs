bf3c22a642223b282ed255252e94f17ef7c62272
Ilya Morin <moy.morin22@gmail.com>
2022-07-13 13:39:45 +0300

    Create Creature.cs
    Create Creature class and add fields and properties: Name, Health, Damage

---
+++ Creature.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTest
+{
+    public class Creature
+    {
+        private string name = string.Empty;
+        private int health;
+        private int health_limit = 100;
+        private int damage;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    name = value;
+            }
+        }
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                if (value > health_limit)
+                    health = health_limit;
+                else
+                if (value <= 0)
+                    health = 0;
+                else
+                    health = value;
+            }
+        }
+        public int Damage
+        {
+            get { return damage; }
+            set
+            {
+                if (value <= 0)
+                    damage = 0;
+                else
+                    damage = value;
+            }
+        }
+    }
+}
